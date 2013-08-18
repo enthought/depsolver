ffe1b0bfb92d91bedb94294e93bd0c9c033d599d
David Cournapeau <cournape@gmail.com>
2013-08-18 18:42:47 +0100

    ENH: change priority convention to follow composer.

--- depsolver/pool.py
+++ depsolver/pool.py
@@ -219,8 +219,11 @@ class Pool(HasTraits):
         """
         Returns the priority of a repository.
 
-        If no constraint has been set up for the repository, its priority is 0
-        (the lowest).
+        Priorities are in the ]-inf, 0] integer range, and the ordering is
+        similar to the Unix nice system: the lower the priority number, the
+        more a repository has priority over other repositories.
+
+        If no constraint has been set up for the repository, its priority is 0.
 
         Parameters
         ----------
@@ -228,6 +231,7 @@ class Pool(HasTraits):
             The repository to compute the priority of.
         """
         if repository.name in self._repository_by_name:
-            return self._scheduler.compute_priority().get(repository.name, 0)
+            # We return a negative number to follow Composer convention.
+            return -self._scheduler.compute_priority().get(repository.name, 0)
         else:
             raise DepSolverError("Unknown repository name '%s'" % (repository.name,))

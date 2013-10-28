9d08c3cdf8c38b5639cdc4fb4887f94e02bd4b41
David Cournapeau <cournape@gmail.com>
2013-10-28 17:02:15 +0100

    MAINT: let's try new rtd style.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -94,10 +94,9 @@ pygments_style = 'tango'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if on_rtd:
-    html_theme = 'default'
-else:
+if not on_rtd:
     html_theme = 'haiku'
+RTD_NEW_THEME = True
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
